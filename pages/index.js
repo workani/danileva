e84974e48caba9f07dfe5b51512f0d9599082d5e
danilminer234 <42529310+danilminer234@users.noreply.github.com>
2024-01-10 18:11:26 +0100

    buttonsss & auto scrolling time

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ export default function CombinedComponent() {
     changeImage();
     setTimeout(() => {
       setActiveButton(null);
-    }, 500); // Кнопка будет красной в течение 500 мс
+    }, 1000); // Кнопка будет красной в течение 500 мс
   };
 
 
@@ -61,7 +61,7 @@ export default function CombinedComponent() {
       interval = setInterval(() => {
         setCurrentIndex(prevIndex => (prevIndex + 1) % images.length);
         setImageLoaded(false); // Reset the image loaded state for fade-in effect
-      }, 4000); // Change every 4 seconds
+      }, 3300); // Change every 4 seconds
     }
 
     return () => clearInterval(interval);
